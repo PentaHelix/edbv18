65c83d0c09a79de6e74434f7a0b06167c3dd2781
saaraahK <s.kuchler@gmx.at>
2019-01-05 05:08:02 +0100

    commands changed

--- morphology.m
+++ morphology.m
@@ -1,8 +1,11 @@
 %Author: Sarah Kuchler 11776811
 function [img] = morphology(img)
     img = ~img;
-    cleanElement = [0 0 0; 0 1 0; 0 0 0];                   %structure element with 8 neighbors
-    fillElement = [1 1 1;1 0 1;1 1 1];
+    
+    %structure elements with 8 neighbors
+    %fill = fills holes; clean = deletes single pixels
+    cleanElement = [0 0 0; 0 1 0; 0 0 0];
+    fillElement = [1 1 1; 1 0 1; 1 1 1];
     
     img = compareElement(img,cleanElement,0);
     img = compareElement(img,fillElement,1);
@@ -10,21 +13,18 @@ end
 
 %Author: Sarah Kuchler 11776811
 function [imgNew] = compareElement(img,Element,value)
-    imgOld = img;
     imgNew = img;
-    sz = size(imgOld);
+    sz = size(imgNew);
     
-    %while true                                             %do-while loop
-        for i=2:sz(1)-1                                     %rows
-            for j=2:sz(2)-1                                 %columns
-                if isequal(imgOld(i-1:i+1,j-1:j+1),Element) %compares (same size) part of the picture with the structure element
-                   imgNew(i,j) = value;                     %Changes value of the pixel in the middle 
-                end
-            end
-        end
-        %if ~isequal(imgOld,imgNew)                          %Checks if something has changed after comparing with the structure element
-            %break                                           %if so there will be no further changes -> stop while loop
-        %end
-        %imgOld = imgNew;
-    %end
+    %compares the structure element with (same size) part of the picture
+    %ignores the outermost pixels (starts at the pixel in the second row
+    %and column)
+    for i=2:sz(1)-1                                    
+       for j=2:sz(2)-1                                 
+           if isequal(imgNew(i-1:i+1,j-1:j+1),Element) 
+              %Changes value of the pixel in the middle (hot spot) 
+              imgNew(i,j) = value;                     
+           end
+       end
+    end
 end
