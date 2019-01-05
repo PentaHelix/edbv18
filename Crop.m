af0ad36f8cb3c8bb575ed96ad5cbc8cefe4748b7
Jakob Kuen <jakkuen@gmail.com>
2019-01-05 23:31:35 +0100

    Added comments

--- Crop.m
+++ Crop.m
@@ -66,37 +66,55 @@ end
 
 %Author: Jakob
 function [stats] = components(Image)
+    % convert image to logical
     Image = logical(Image(:,:,1));
     stats = [];
     [w, h]=size(Image);
+    % matrix that holds information about which pixel belongs to which
+    % component
     comps = zeros(w,h);
+    % first components = id 1
     counter = 1;
 
+    % loop through all pixels
     for i = 1:w
         for j = 1:h
+            
             if(Image(i,j) == 1)
                  idx = ((j-1)*w + i);
                  comps(idx) = counter;
+                 % default bounding box to be the size of the whole image,
+                 % but inverted
                  bb.x0 = h;
                  bb.y0 = w;
                  bb.x1 = 0;
                  bb.y1 = 0;
                  while ~isempty(idx)
                       Image(idx)=0;
-                      
+                       
+                      % get the neighbors of all the pixels from the previous
+                      % iteration
                       neighbors = idx + [-1; w; 1; -w]';
+                      % clamp possible pixel values so that there are no
+                      % pixels outside of the image bounds
                       neighbors = neighbors(neighbors > 0 & neighbors < w * h);
+                      % remove dupliace pixels
                       neighbors = unique(neighbors(:));
-
+                        
+                      % remove pixels which are not white in the image
                       idx = neighbors(Image(neighbors));
                       comps(idx)=counter;
                       
+                      % get the minimum and maximum pixel ofthe current
+                      % selection
                       minY = min(mod(idx, w));
                       minX = min(floor(idx / w));
                       
                       maxY = max(mod(idx, w));
                       maxX = max(floor(idx / w));
                       
+                      % if the currently considered pixels are outside of
+                      % the bounding box, increase the bounding box size 
                       if (minX < bb.x0) bb.x0 = minX; end
                       if (minY < bb.y0) bb.y0 = minY; end
                       if (maxX > bb.x1) bb.x1 = maxX; end
@@ -104,12 +122,18 @@ function [stats] = components(Image)
                       
                  end
                  
+                 % set stats for the component with index 'counter'
                  stats(counter).Index = counter;
+                 % bounding box is calculated in the loop, just convert it to
+                 % the format used in the following pipeline
                  stats(counter).BoundingBox = [bb.x0, bb.y0, bb.x1 - bb.x0, bb.y1 - bb.y0];
+                 %% calculate area of/from bounding box
                  stats(counter).Area = (bb.x1 - bb.x0) * (bb.y1 - bb.y0);
                  
+                 % get pixel indices of component
                  pixels = find(comps == counter);
                  
+                 % calculate centroid (mean of all pixel coordinates)
                  stats(counter).Centroid = floor([mean(floor(pixels / h)), mean(mod(pixels, w))]);
                  counter = counter + 1;
             end
